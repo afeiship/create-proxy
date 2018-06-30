ff15c91513cda5972c9afcc6a840825c4a8f4aea
afeiship <1290657123@qq.com>
2018-06-30 18:10:31 +0800

    issue: context fix

--- dist/next-abstract-storage.js
+++ dist/next-abstract-storage.js
@@ -1,6 +1,6 @@
 (function () {
 
-  var global = global || this;
+  var global = global || window || self || Function('return this')();
   var nx = global.nx || require('next-js-core2');
   var EMPTY_STR = '';
   var DOT = '.';
